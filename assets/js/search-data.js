a677053f3103e8ff73cacb43817f5fa01982667a
h-jpj <113398226+h-jpj@users.noreply.github.com>
2025-02-08 17:52:28 +0000

    Deploying to gh-pages from @ h-jpj/h-jpj.github.io@ab7d1b28a5c3b446a8a8110678a8bde32d406138 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -457,9 +457,9 @@ ninja.data = [{
           description: "My end of (3rd) year disseration project for my Mechatronics and Robotics MEng degree.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-advancing-stroke-rehabilitation-a-neuroplasticity-driven-approach",
+          title: 'Advancing Stroke Rehabilitation, a Neuroplasticity-Driven Approach',
+          description: "My end of (3rd) year disseration project for my Mechatronics and Robotics MEng degree.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
