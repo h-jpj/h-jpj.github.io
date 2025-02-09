207cc083daad11d7564f5c3b6a49984191080a1e
h-jpj <113398226+h-jpj@users.noreply.github.com>
2025-02-09 19:20:29 +0000

    Deploying to gh-pages from @ h-jpj/h-jpj.github.io@6834b834b63a1d5ae95af04967ec3ef89f68aeff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "dropdown-publications",
               title: "publications",
               description: "",
@@ -138,29 +131,9 @@ ninja.data = [{
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
-          description: "a project with a background image",
+          description: "Exoskeleton Rehabilitation Arm.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
