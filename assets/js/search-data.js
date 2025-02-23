19a14da37ec0dbbb6ed6055b5ec20b2b04e4f751
h-jpj <113398226+h-jpj@users.noreply.github.com>
2025-02-23 15:56:43 +0000

    Deploying to gh-pages from @ h-jpj/h-jpj.github.io@2f948269c6347a6295b51d20d287fc4f6d851db7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Here is a slim version of my CV. You can download the full PDF from the top right document icon on this page.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
